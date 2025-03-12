38bbb310aea525388ef8b6f6a090f4178b578951
Promise Munisi <munisipromise@egmail.com>
2025-03-12 19:00:10 +0200

    plp python-assignment1

---
+++ python_program.py
@@ -0,0 +1,26 @@
+def perform_operation():
+    
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    operation = input("Enter the operation (+, -, *, /): ")
+
+    
+    if operation == "+":
+        result = num1 + num2
+    elif operation == "-":
+        result = num1 - num2
+    elif operation == "*":
+        result = num1 * num2
+    elif operation == "/":
+        if num2 != 0:
+            result = num1 / num2
+        else:
+            result = "Error! you cannot divide by zero."
+    else:
+        result = "Invalid operation."
+
+    
+    print(f"{num1} {operation} {num2} = {result}")
+
+
+perform_operation()
